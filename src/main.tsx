6277757d33628b049fe405dbaac752b6608370ea
fabriciosfernandez-web <fabriciosfernandez@gmail.com>
2026-09-16 14:03:39 -0300

    feat(ui): wire theme provider and enterprise stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { ThemeProvider } from './theme/ThemeProvider'
 import './styles.css'
 import './admin-ui.css'
+import './theme.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )

9c0d8c171a7a5412a66be6400cbebfa4954bbff8
fabriciosfernandez-web <fabriciosfernandez@gmail.com>
2026-09-25 07:40:43 -0300

    feat(ops): protect app with global error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { GlobalErrorBoundary } from './components/GlobalErrorBoundary'
 import { ThemeProvider } from './theme/ThemeProvider'
 import './styles.css'
 import './admin-ui.css'
@@ -10,11 +11,14 @@ import './dark-theme-polish.css'
 import './notifications-ui.css'
 import './push-dev.css'
 import './loading-screen.css'
+import './app-error.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <GlobalErrorBoundary>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </GlobalErrorBoundary>
   </StrictMode>,
 )
